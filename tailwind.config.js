a391d8149ab9c290f463a2ea95ac4846f3ff36c4
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 <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>
2022-03-25 13:53:13 +0100

    Add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: '#1f1f38',
+        bgAlt: '#2c2c6c',
+        primary: '#4db5ff66',
+        primaryAlt: '#4db5ff',
+        light: '#ffffff99'
+      },
+      backgroundImage: {
+        'bg-texture': "url('../src/assets/bg-texture.png')",
+      },
+    },
   },
   plugins: [],
 }
