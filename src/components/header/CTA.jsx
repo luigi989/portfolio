8581a00af65c5d85b3ce64ca6826d4a3433f3dcc
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 <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>
2022-03-28 12:21:00 +0200

    Add tailwind css

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,9 +3,17 @@ import CV from '../../assets/cv.pdf';
 
 const CTA = () => {
   return (
-    <div className='cta'>
-        <a href={CV} download>Download CV</a>
-        <a href="#contact">Let's talk</a>
+    <div className='mt-10 flex gap-5 justify-center'>
+        <a className='hover:bg-light hover:text-white hover:border-transparent 
+                      transition ease-linear duration-300
+                      w-max inline-block py-3 px-5 rounded-lg cursor-pointer border-solid border-2
+                      border-primary text-primaryAlt' 
+           href={CV} download>Download CV</a>
+        <a className='hover:text-bg hover:bg-white hover:border-transparent 
+                      transition ease-linear duration-300
+                      w-max inline-block py-3 px-5 rounded-lg cursor-pointer border-solid border-2 
+                      border-primary bg-primaryAlt text-bg' 
+           href="#contact">Let's talk</a>
     </div>
   )
 }
