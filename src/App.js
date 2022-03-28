fd5dd7989356e8b38da24f21ad5420005afe1211
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 <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>
2022-03-28 15:08:42 +0200

    Fix layout issue

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <div className="">
+    <div className="relative">
       <>
         <Header />
         <Nav />
