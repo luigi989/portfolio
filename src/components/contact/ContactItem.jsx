92e12d7efecea794e04bbae50ba789dc8bc73511
Luigi989 <ludde.lindahl@gmail.com>
2022-03-30 01:24:44 +0200

    Initial commit

---
+++ src/components/contact/ContactItem.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FiMail } from 'react-icons/fi';
+
+const ContactItem = (props) => {
+  return (
+    <article className='bg-bgAlt border-solid border-[1px] border-transparent rounded-2xl p-5 lg:p-6 text-center
+            hover:bg-transparent hover:border-primary hover:cursor-default transition ease-linear duration-300
+            flex flex-col'>
+        {props.children}
+        <h5 className='text-base'>{props.header}</h5>
+        <small className='text-xs text-light mb-4'>{props.contact}</small> 
+        <a href='' className='text-xs'>Send a Message</a>
+    </article>
+  )
+}
+
+export default ContactItem;
