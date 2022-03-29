fccb170d36b1114fbc12e09b7abd0458f47c598a
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 <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>
2022-03-29 16:14:52 +0200

    Add content

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import ServiceItem from './ServiceItem';
+import ServiceList from './ServiceList';
 
 const Services = () => {
   return (
-    <section id='services' className='mt-32 h-screen'>
-      Services
+    <section id='services' className='mt-32h-fit lg:h-screen pt-4 lg:pt-8'>
+      <h5 className='text-light font-medium'>What I Offer</h5>
+      <h2 className='text-primaryAlt font-medium text-3xl lg:mb-32'>Services</h2>
+
+      <div className='w-3/4 m-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 services_container'>
+        <ServiceList title='UI/UX Design'>
+          <ServiceItem text='Lorem ipsum dolor, sit amet consectetur adipisicing elit' />
+          <ServiceItem text='Lorem ipsum dolor, sit amet consectetur adipisicing elit' />
+          <ServiceItem text='Lorem ipsum dolor, sit amet consectetur adipisicing elit' />
+          <ServiceItem text='Lorem ipsum dolor, sit amet consectetur adipisicing elit' />
+          <ServiceItem text='Lorem ipsum dolor, sit amet consectetur adipisicing elit' />
+        </ServiceList>
+
+        <ServiceList title='Web Development'>
+          <ServiceItem text='Lorem ipsum dolor, sit amet consectetur adipisicing elit' />
+          <ServiceItem text='Lorem ipsum dolor, sit amet consectetur adipisicing elit' />
+          <ServiceItem text='Lorem ipsum dolor, sit amet consectetur adipisicing elit' />
+          <ServiceItem text='Lorem ipsum dolor, sit amet consectetur adipisicing elit' />
+          <ServiceItem text='Lorem ipsum dolor, sit amet consectetur adipisicing elit' />
+          <ServiceItem text='Lorem ipsum dolor, sit amet consectetur adipisicing elit' />
+        </ServiceList>
+
+        <ServiceList title='Content Creation'>
+          <ServiceItem text='Lorem ipsum dolor, sit amet consectetur adipisicing elit' />
+          <ServiceItem text='Lorem ipsum dolor, sit amet consectetur adipisicing elit' />
+          <ServiceItem text='Lorem ipsum dolor, sit amet consectetur adipisicing elit' />
+          <ServiceItem text='Lorem ipsum dolor, sit amet consectetur adipisicing elit' />
+        </ServiceList>
+      </div>
     </section>
   )
 }
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Avatar1 from '../../assets/avatar1.jpg';
+import Avatar2 from '../../assets/avatar2.jpg';
+import Avatar3 from '../../assets/avatar3.jpg';
+import Avatar4 from '../../assets/avatar4.jpg';
+import AvatarCard from './AvatarCard';
 
 const Testimonials = () => {
+  const text = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est, doloremque.' + 
+    'Officiis iste laudantium nemo, porro, rem recusandae maxime nobis laboriosam ' +
+    'beatae in a eligendi suscipit odio perspiciatis similique, molestias sapiente.';
+
   return (
-    <section id='testimonials' className='mt-32 h-screen'>Testimonials</section>
+    <section id='testimonials' className='mt-32 h-fit lg:h-screen pt-4 lg:pt-8'>
+      <h5 className='text-light font-medium'>Review from clients</h5>
+      <h2 className='text-primaryAlt font-medium text-3xl lg:mb-16'>Testimonials</h2>  
+
+      <div className='w-1/4 m-auto testimonials_container'>
+        <AvatarCard name='Ernst Achiever' text={text} path={Avatar1} />
+        <AvatarCard name='Ernst Achiever' text={text} path={Avatar2} />
+        <AvatarCard name='Ernst Achiever' text={text} path={Avatar3} />
+        <AvatarCard name='Ernst Achiever' text={text} path={Avatar4} />
+
+      </div>
+    </section>
   )
 }
 
