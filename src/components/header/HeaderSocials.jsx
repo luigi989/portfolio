a0bad4e6f0b4e499726e7c4c4c90ce88de5a2e64
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 <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>
2022-03-30 21:10:03 +0200

    Add links to my profiles

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,10 +6,11 @@ const HeaderSocials = () => {
     <div className="hidden lg:flex flex-col items-center gap-3 absolute left-0 bottom-12
                     after:content-[''] after:w-[1px] after:h-8 after:bg-primaryAlt">
         <a className='inline-block text-primaryAlt hover:text-white transition ease-linear duration-300' 
-            href='https://linkedin.com' target='_blank' rel='noreferrer'><BsLinkedin /></a>
+            href='https://www.linkedin.com/in/ludvig-lindahl/' target='_blank' rel='noreferrer'><BsLinkedin /></a>
         <a className='inline-block text-primaryAlt hover:text-white transition ease-linear duration-300'
-            href='https://github.com' target='_blank' rel='noreferrer'><BsGithub /></a>
+            href='https://github.com/luigi989' target='_blank' rel='noreferrer'><BsGithub /></a>
         <a className='inline-block text-primaryAlt hover:text-white transition ease-linear duration-300'
+        // Here my portfolio dashboard shoule be???
             href='https://dribble.com' target='_blank' rel='noreferrer'><BsDribbble /></a>
     </div>
   )
