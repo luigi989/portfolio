ebe48193e2a5bd23ca62d48eb37e32433404573a
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 <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>
2022-04-11 16:54:09 +0200

    Add focus rules for the text fields

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -23,27 +23,27 @@ const Contact = () => {
       
       <div className='w-3/4 lg:w-2/4 m-auto flex flex-col md:flex-row gap-16 lg:gap-32'>
         <div className='flex flex-col gap-6'>
-          <ContactItem header='Email' contact='ludde.lindahl@gmail.com'> 
+          <ContactItem header='Email' contact='ludde.lindahl@gmail.com' href='mailto:ludde.lindahl@gmail.com'> 
             <FiMail className='m-auto text-2xl mb-4' />
           </ContactItem>
-          <ContactItem header='Messenger' contact='Ludvig Lindahl'>
+          <ContactItem header='Messenger' contact='Ludvig Lindahl' href='https://m.me/ludvig.lindahl.1'>
             <RiMessengerLine className='m-auto text-2xl mb-4' />
           </ContactItem>
-          <ContactItem header='WhatsApp' contact='Phone number'>
+          <ContactItem header='WhatsApp' contact='+46705789618' href='https://wa.me/46705789618'>
             <FaWhatsapp className='m-auto text-2xl mb-4' />
           </ContactItem>
         </div>
 
         <form ref={form} onSubmit={sendEmail} className='flex flex-col gap-4 w-full'>
           <input type='text' name='name' placeholder='Your Full Name' required
-            className='bg-transparent border-solid border-[1px] border-primary p-5 rounded-xl w-full' />
+            className='bg-transparent border-solid border-[1px] border-primary p-5 rounded-xl w-full focus:bg-bgAlt duratio-300' />
           <input type='email' name='email' placeholder='Your Email' required
-            className='bg-transparent border-solid border-[1px] border-primary p-5 rounded-xl w-full' />
+            className='bg-transparent border-solid border-[1px] border-primary p-5 rounded-xl w-full focus:bg-bgAlt duration-300' />
           <textarea name='message' rows='7' placeholder='Your Message' required
-            className='bg-transparent border-solid border-[1px] border-primary p-5 rounded-xl resize-none w-full'></textarea>
-          <button type='submit' className='text-bg bg-primaryAlt transition ease-linear duration-300
-                                        w-max inline-block py-3 px-5 cursor-pointer rounded-md border-solid border-2 border-primaryAlt 
-                                        hover:text-primary hover:bg-bgAlt'>Send Message</button>
+            className='bg-transparent border-solid border-[1px] border-primary p-5 rounded-xl resize-none w-full focus:bg-bgAlt duration-300' />
+          <button type='submit' className='self-end text-bg bg-primaryAlt transition ease-linear duration-300
+                                        w-max inline-block py-3 px-5 cursor-pointer rounded-md border-solid border-2
+                                        hover:text-primary hover:bg-bgAlt focus:text-primary focus:bg-bgAlt'>Send</button>
         </form>
       </div>
     </section>
