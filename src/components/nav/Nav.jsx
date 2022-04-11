1daaf578ffd7dac5304fcebba5bcb519998a55d8
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 <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>
2022-04-11 16:52:01 +0200

    Small code changes

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,42 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
 import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai';
 import { BiBook, BiMessageSquareDetail } from 'react-icons/bi';
-import { BsBriefcase } from 'react-icons/bs';
+import { BsBriefcase, BsWindowSidebar } from 'react-icons/bs';
 
 const Nav = () => {
-  const [activeNav, setActiveNav] = useState('#');
+   const [activeNav, setActiveNav] = useState('#');
 
-  const navClass = "text-primaryAlt hover:bg-black hover:bg-opacity-40" +
-                    "bg-transparent p-[0.9rem] rounded-[50%] flex text-light text-lg " +
-                    "transition ease-linear duration-300"
-  const active = " bg-primaryAlt text-white";
+   const navClass = "text-primaryAlt hover:bg-black hover:bg-opacity-40" +
+      "bg-transparent p-[0.9rem] rounded-[50%] flex text-light text-lg " +
+      "transition ease-linear duration-300"
+   const active = " bg-primaryAlt text-white";
 
-  return (
-    <nav className="bg-black bg-opacity-50 w-max py-3 px-7 fixed left-1/2 -translate-x-1/2 bottom-8
+   return (
+      <nav className="bg-black bg-opacity-50 w-max py-3 px-7 fixed left-1/2 -translate-x-1/2 bottom-8
                     flex gap-3 rounded-[3rem] backdrop-blur-lg">
 
-      <a className={activeNav === '#' ? navClass + active: navClass} 
-         href='/#'
-         onClick={() => setActiveNav('#')}><AiOutlineHome /></a>
-                    
-      <a className={activeNav === '#about' ? navClass + active : navClass} 
-         href='#about'
-         onClick={() => setActiveNav('#about')}><AiOutlineUser /></a>
+         <a className={activeNav === '#' ? navClass + active : navClass}
+            href='/#'
+            onClick={() => setActiveNav('#')}><AiOutlineHome /></a>
 
-      <a className={activeNav === '#experience' ? navClass + active : navClass} 
-         href='#experience'
-         onClick={() => setActiveNav('#experience')}><BiBook /></a>
+         <a className={activeNav === '#about' ? navClass + active : navClass}
+            href='#about'
+            onClick={() => setActiveNav('#about')}><AiOutlineUser /></a>
 
-      <a className={activeNav === '#portfolio' ? navClass + active : navClass} 
-         href='#portfolio'
-         onClick={() => setActiveNav('#portfolio')}><BsBriefcase /></a>
-                    
-      <a className={activeNav === '#contact' ? navClass + active : navClass} 
-         href='#contact'
-         onClick={() => setActiveNav('#contact')}><BiMessageSquareDetail /></a>
-    </nav>
-  )
+         <a className={activeNav === '#experience' ? navClass + active : navClass}
+            href='#experience'
+            onClick={() => setActiveNav('#experience')}><BiBook /></a>
+
+         <a className={activeNav === '#portfolio' ? navClass + active : navClass}
+            href='#portfolio'
+            onClick={() => setActiveNav('#portfolio')}><BsBriefcase /></a>
+
+         <a className={activeNav === '#contact' ? navClass + active : navClass}
+            href='#contact'
+            onClick={() => setActiveNav('#contact')}><BiMessageSquareDetail /></a>
+      </nav>
+   )
 }
 
 export default Nav
