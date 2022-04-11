1d0dd35ac96e9dc31d4f67a84467b268d3dccbb3
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 <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>
2022-04-11 16:50:58 +0200

    Remove tag

--- src/components/portfolio/PortfolioCard.jsx
+++ src/components/portfolio/PortfolioCard.jsx
@@ -13,7 +13,7 @@ const PortfolioCard = (props) => {
                 transition ease-linear duration-300
                 w-max inline-block py-1 px-3 rounded-lg cursor-pointer border-solid border-[1px]
                 border-primaryAlt text-primaryAlt'
-                    href={props.link ? props.link : 'https://github.com/luigi989'} target='_blank' rel='noreferrer' download>Github</a>
+                    href={props.link ? props.link : 'https://github.com/luigi989'} target='_blank' rel='noreferrer'>Github</a>
                 <a className='hover:text-light hover:bg-bgAlt transition ease-linear duration-300
                 w-max inline-block py-1 px-3 rounded-lg cursor-pointer border-solid border-[1px] 
                 border-primary bg-primaryAlt text-bg'
