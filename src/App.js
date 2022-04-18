5513c6f0fbdf06460d62a4bdf2623aecf0b63f4e
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 <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>
2022-04-18 11:07:05 +0200

    Pass snap points to each scrollable component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useEffect, useRef } from 'react';
+// import { useState, useEffect, useRef } from 'react';
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
 import About from './components/about/About';
@@ -9,58 +9,53 @@ import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from 'recoil';
+// import {
+//   RecoilRoot,
+//   atom,
+//   selector,
+//   useRecoilState,
+//   useRecoilValue,
+// } from 'recoil';
 
 function App() {
-  const ref = useRef();
-  const ref2 = useRef();
-  const onScreen = useOnScreen(ref, "-300px", "about");
-  const onScreen2 = useOnScreen(ref2, "-300px", "portfolio");
+  // const ref = useRef();
+  // const ref2 = useRef();
+  // const onScreen = useOnScreen(ref, "-300px", "about");
+  // const onScreen2 = useOnScreen(ref2, "-300px", "portfolio");
 
-  function useOnScreen(ref, rootMargin = "0px", message) {
-    const [isIntersecting, setIntersecting] = useState(false);
+  // function useOnScreen(ref, rootMargin = "0px", message) {
+  //   const [isIntersecting, setIntersecting] = useState(false);
 
-    useEffect(() => {
-      const observer = new IntersectionObserver(
-        ([entry]) => {
-          setIntersecting(entry.isIntersecting);
-        },
-        {
-          rootMargin,
-        }
-      );
-      if (ref.current) {
-        observer.observe(ref.current);
-      }
-      return () => {
-        observer.unobserve(ref.current);
-      };
-    }, []);
-    console.log(message);
-    return isIntersecting;
-  }
+  //   useEffect(() => {
+  //     const observer = new IntersectionObserver(
+  //       ([entry]) => {
+  //         setIntersecting(entry.isIntersecting);
+  //       },
+  //       {
+  //         rootMargin,
+  //       }
+  //     );
+  //     if (ref.current) {
+  //       observer.observe(ref.current);
+  //     }
+  //     return () => {
+  //       observer.unobserve(ref.current);
+  //     };
+  //   }, []);
+  //   console.log(message);
+  //   return isIntersecting;
+  // }
 
   return (
-    <div className="relative">
-      <>
-        <RecoilRoot>
-          <Header />
-          <About inView={ref}/>
-          <Experience />
-          {/* <Services /> */}
-          <Portfolio inView={ref2}/>
-          <Testimonials />
-          <Contact />
-          <Footer />
-          <Nav />
-        </RecoilRoot>
-      </>
+    <div>
+      <Header snap='lg:snap-start' />
+      <About snap='lg:snap-start' />
+      <Experience snap='lg:snap-start' />
+      <Portfolio snap='lg:snap-start' />
+      <Testimonials snap='lg:snap-start' />
+      <Contact snap='lg:snap-start' />
+      <Footer snap='lg:snap-start' />
+      <div><Nav /></div>
     </div>
   );
 }
