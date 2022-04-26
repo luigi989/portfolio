37babd613f48dd61fb7c7d8490eb873b66cf024d
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 <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>
2022-04-26 09:20:10 +0200

    Initial commit

---
+++ src/components/contact/Error.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Error = ({ children }) => {
+    return (
+        <span className='mt-1 label-text-alt text-error'>{children}</span>
+    )
+}
+export default Error
