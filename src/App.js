4bb933a141881194824e71a1852a57bdd24e71c6
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 <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>
2022-04-26 14:58:22 +0200

    Fix tailwind css

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
+import { useRecoilValue } from 'recoil';
 import './App.css';
+import { darkModeState } from './atoms/atoms';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Experience from './components/experience/Experience';
+import Footer from './components/footer/Footer';
 // import { useState, useEffect, useRef } from 'react';
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
-import About from './components/about/About';
-import Experience from './components/experience/Experience';
 import Portfolio from './components/portfolio/Portfolio';
 import Testimonials from './components/testimonials/Testimonials';
-import Contact from './components/contact/Contact';
-import Footer from './components/footer/Footer';  
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from 'recoil';
-import { darkModeState } from './atoms/atoms';
 
 function App() {
   const isDark = useRecoilValue(darkModeState);
-  console.log(isDark)
 
   return (
-    <div className={isDark ? 'bg-bgTexture ' : 'lightPattern ' + 'bg-liBg text-black dark:bg-bg dark:bg-bgTexture dark:text-white'}>
+    <div className={isDark ? 'bg-bgTexture bg-liBg text-black dark:bg-bg dark:text-white' : 
+                              'lightPattern bg-liBg text-black dark:bg-bg dark:text-white'}>
       <Header snap='lg:snap-start' />
       <About snap='lg:snap-start' />
       <Experience snap='lg:snap-start' />
