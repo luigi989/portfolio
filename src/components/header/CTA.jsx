a6054fe0f321411cb8b15e544e68c4e05854308e
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 <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>
2022-04-26 14:50:59 +0200

    Imbed a link in a button

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,14 +4,15 @@ import Button from '../shared/Button';
 
 const CTA = () => {
   return (
-    <div className='mt-10 flex gap-5 justify-center'>
-        <a className='dark:hover:bg-bgAlt dark:hover:text-white dark:hover:border-transparent dark:border-primary dark:text-primaryAlt dark:bg-none
+    <div className='mt-[37px] flex gap-5 justify-center'>
+      <button className='dark:hover:bg-bgAlt dark:hover:text-white dark:hover:border-transparent dark:border-primary dark:text-primaryAlt dark:bg-none
                       border-solid border-2
                       border-transparent text-liSec border-liSec hover:bg-liSec hover:text-liBg
                       transition ease-linear duration-300
-                      w-max inline-block py-3 px-5 rounded-lg cursor-pointer text-center'
-           href={CV} download>Download CV</a>
-        <Button link={"#contact"}>Let's talk</Button>
+                      py-3 px-5 rounded-lg cursor-pointer text-center'>
+        <a href={CV} download>Download CV</a>
+      </button>
+      <Button link={"#contact"}>Let's talk</Button>
     </div>
   )
 }
