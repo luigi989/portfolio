be0893b90075c2faf459a073acc94f97f1525c0e
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 <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>
2022-04-29 15:57:48 +0200

    Add nav state atoms

--- src/atoms/atoms.js
+++ src/atoms/atoms.js
@@ -4,3 +4,35 @@ export const darkModeState = atom({
     key: 'darkModeState',
     default:'true',
  });
+
+ export const headerVisibleState = atom({
+    key: 'headerVisibleState',
+    default: 'false',
+})
+ 
+export const aboutVisibleState = atom({
+     key: 'aboutVisibleState',
+     default: 'false',
+ })
+
+
+export const experienceVisibleState = atom({
+    key: 'experienceVisibleState',
+    default: 'false',
+})
+
+export const portfolioVisibleState = atom({
+    key: 'portfolioVisibleState',
+    default: 'false',
+})
+
+export const sectionVisibleState = atom({
+    key: 'sectionVisibleState',
+    default: {
+        header: false,
+        about: false,
+        experiences: false,
+        portfolio: false,
+        contact: false,
+    }
+})
