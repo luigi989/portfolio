12c1c4fc770bca435812fe542758585d2a9c56ca
Luigi989 <ludde.lindahl@gmail.com>
2022-04-29 18:07:58 +0200

    Removed unused atoms

--- src/atoms/atoms.js
+++ src/atoms/atoms.js
@@ -5,33 +5,12 @@ export const darkModeState = atom({
     default:'true',
  });
 
- export const headerVisibleState = atom({
-    key: 'headerVisibleState',
-    default: 'false',
-})
- 
-export const aboutVisibleState = atom({
-     key: 'aboutVisibleState',
-     default: 'false',
- })
-
-
-export const experienceVisibleState = atom({
-    key: 'experienceVisibleState',
-    default: 'false',
-})
-
-export const portfolioVisibleState = atom({
-    key: 'portfolioVisibleState',
-    default: 'false',
-})
-
 export const sectionVisibleState = atom({
     key: 'sectionVisibleState',
     default: {
         header: false,
         about: false,
-        experiences: false,
+        experience: false,
         portfolio: false,
         contact: false,
     }
