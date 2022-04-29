b20eeec694ef81b83cc6703fdc6df6a5533121b7
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 <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>
2022-04-29 15:59:08 +0200

    Add WIP nav state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
-import { useRecoilValue } from 'recoil';
-import './App.css';
-import { darkModeState } from './atoms/atoms';
+import { useRecoilState, useRecoilValue } from 'recoil';
+import { useInView } from "react-cool-inview";
+import { useEffect } from 'react';
+import { darkModeState, sectionVisibleState } from './atoms/atoms';
 import About from './components/about/About';
 import Contact from './components/contact/Contact';
 import Experience from './components/experience/Experience';
 import Footer from './components/footer/Footer';
-// import { useState, useEffect, useRef } from 'react';
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
 import Portfolio from './components/portfolio/Portfolio';
-import Testimonials from './components/testimonials/Testimonials';
+import './App.css';
 
 function App() {
   const isDark = useRecoilValue(darkModeState);
 
+  const [sectionsVisible, setSectionsVisible] = useRecoilState(sectionVisibleState);
+
+  const useView = () => {
+    const { observe, inView} = useInView({
+      threshold: 1,
+      onChange: ({ inView, scrollDirection }) => {
+        if (inView) {
+          console.log(scrollDirection.vertical);
+        }
+      },
+    })
+
+    return { observe, inView };
+  }
+
+  const { observe: observeAbout, inView: inViewAbout, scrollDirection: scrollAbout } = useView();
+  const { observe: observeExperience, inView: inViewExperince } = useView();
+
+  useEffect(() => {
+    if (inViewAbout) {
+      console.log('About visible');
+      // console.log(scrollAbout);
+      const sections = sectionsVisible;
+
+      console.log(sections);
+    }
+  }, [observeAbout, inViewAbout, sectionsVisible, scrollAbout])
+
+  // useEffect(() => {
+  //   if (inViewExperince) {
+  //     console.log('Experience visible');
+  //     console.log(observeExperience);
+  //   }
+  // }, [observeExperience, inViewExperince, changeVisibleState])
+
+
   return (
-    <div className={isDark ? 'bg-bgTexture bg-liBg text-black dark:bg-bg dark:text-white' : 
-                              'lightPattern bg-liBg text-black dark:bg-bg dark:text-white'}>
+    <div className={isDark ? 'bg-bgTexture bg-liBg text-black dark:bg-bg dark:text-white' :
+      'lightPattern bg-liBg text-black dark:bg-bg dark:text-white'}>
       <Header snap='lg:snap-start' />
-      <About snap='lg:snap-start' />
-      <Experience snap='lg:snap-start' />
+      <About visibilityRef={observeAbout} snap='lg:snap-start' />
+      <Experience visibilityRef={observeExperience} snap='lg:snap-start' />
       <Portfolio snap='lg:snap-start' />
       <Contact snap='lg:snap-start' />
-      <Footer snap='lg:snap-start' />
+      <Footer snap='lg:snap-end' />
       <div><Nav /></div>
     </div>
   );
