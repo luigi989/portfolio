c18ac6e1f7e7008d45f07055161dacc2e63ae23a
Ludvig Lindahl <ludde.lindahl@gmail.com>
2023-02-02 11:16:47 +0100

    updated CV and fixed button click

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -5,20 +5,22 @@ import Button from '../shared/Button';
 const CTA = () => {
   return (
     <div className='mt-[37px] flex gap-5 justify-center'>
-      <button className='bg-liBg dark:bg-transparent dark:hover:bg-bgAlt dark:hover:text-white 
+      <a className='bg-border-primary' href={CV} download>
+        <button className='bg-liBg dark:bg-transparent dark:hover:bg-bgAlt dark:hover:text-white 
                       dark:hover:border-transparent dark:border-primary dark:text-primaryAlt dark:bg-none
                       border-solid border-2
                       border-transparent text-liSec border-liSec hover:bg-liSec hover:text-liBg
-                      transition ease-linear duration-300
+                      transition ease-linear dura   tion-300
                       py-3 px-5 rounded-lg cursor-pointer text-center text-sm whitespace-nowrap md:text-lg md:whitespace-normal'
-        title='Download CV'>
-        <a href={CV} download>Download CV</a>
-      </button>
-      <Button 
+          title='Download CV'>
+          Download CV
+        </button>
+      </a>
+      <Button
         link={"#contact"}
         title='Contact me'
       >
-          Let's talk</Button>
+        Let's talk</Button>
     </div>
   )
 }
