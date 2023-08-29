9575ad06107f70269e32bffc3e7bab3e81a4da47
Ludvig Lindahl <ludde.lindahl@gmail.com>
2023-08-29 14:54:42 +0200

    improve hover effect in light mode

--- src/components/contact/ContactItem.tsx
+++ src/components/contact/ContactItem.tsx
@@ -7,22 +7,34 @@ interface ContactItemProps {
   icon: React.ReactNode;
 }
 
-const ContactItem = ({header, contact, link, icon}: ContactItemProps) => {
+const ContactItem = ({ header, contact, link, icon }: ContactItemProps) => {
   const { t } = useTranslation();
   return (
-    <article className='bg-liSec dark:bg-bgAlt border-solid border-[1px] border-transparent rounded-2xl p-5 lg:p-6 text-center
-            transition ease-linear duration-300 flex flex-col'>
-        <div className="text-liBg dark:text-primaryAlt m-auto text-2xl mb-4">
-          {icon}
-        </div> 
-        <h1 className='text-base text-liBg dark:text-white'>{header}</h1>
-        <small className='mb-4 text-xs text-liBg dark:text-light'>{contact}</small> 
-        <a href={link} target='_blank' rel='noreferrer' 
-          title={t('contact.sendLong') + " " + t('contact.by') + " " + header}
-          aria-label={t('contact.sendLong') + " " + t('contact.by') + " " + header}
-          className='text-sm text-liBg hover:text-liBg dark:text-primaryAlt dark:hover:text-light'>{t('contact.sendLong')}</a>
+    <article
+      className="bg-liSec dark:bg-bgAlt border-solid border-[1px] border-transparent rounded-2xl p-5 lg:p-6 text-center
+            transition ease-linear duration-300 flex flex-col"
+    >
+      <div className="text-liBg dark:text-primaryAlt m-auto text-2xl mb-4">
+        {icon}
+      </div>
+      <h1 className="text-base text-liBg dark:text-white">{header}</h1>
+      <small className="mb-4 text-xs text-liBg dark:text-light">
+        {contact}
+      </small>
+      <a
+        href={link}
+        target="_blank"
+        rel="noreferrer"
+        title={t("contact.sendLong") + " " + t("contact.by") + " " + header}
+        aria-label={
+          t("contact.sendLong") + " " + t("contact.by") + " " + header
+        }
+        className="text-sm text-liBg hover:underline dark:text-primaryAlt dark:hover:text-light dark:hover:no-underline"
+      >
+        {t("contact.sendLong")}
+      </a>
     </article>
-  )
-}
+  );
+};
 
 export default ContactItem;
